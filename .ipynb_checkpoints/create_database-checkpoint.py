5a1dfa6da6d60e41c0883cfed0bc3294d7af9840
Shradha Dinesh <108999498+sdinesh01@users.noreply.github.com>
2023-12-15 18:51:03 -0500

    add comments and docstring

--- .ipynb_checkpoints/create_database-checkpoint.py
+++ .ipynb_checkpoints/create_database-checkpoint.py
@@ -14,16 +14,27 @@ sqlite3.register_adapter(np.int64, lambda val: int(val))
 sqlite3.register_adapter(np.int32, lambda val: int(val))
 
 class MyDB:
+    '''
+    Reads in all .csv files in data/... as a Pandas dataframe and creates a SQLite3 database from a randomly sampled subset   of the dataframe (currently 5 bills per state)
+    
+    class parameters:
+    
+    add_data: add data to the database (default: bool = False)
+    bills_drop: drop tBills table from legislation.db (default: bool = False)
+    any_drop: if any tables is true, rebuild tables (default bool = False)
+    input_lim: this parameter is currently out of use, since we are not creating a database directly with a single csv file. (default: int = None) 
+    chunk_size: this parameter is currently out of use, since we are not creating a database directly with a single csv file. (default: int = None)
+    
+    '''
     def __init__(self,
                  add_data:bool = False,
                  table_type: str = None,
-                 bills_drop: bool = False,
+                 bills_drop: bool = False, 
                  any_drop: bool = False, 
                  input_lim: int = None, 
                  chunk_size: int = None
                 ):
-    
-        #self.path_data = os.path.join(os.path.dirname(__file__), 'sample-data')
+        
         self.path_data = os.path.join(os.path.dirname(__file__), 'data')
         self.path_db = os.path.join(self.path_data, 'legislation.db')
         self.add_data = add_data
@@ -39,16 +50,12 @@ class MyDB:
             self.build_tables()
             print("filling tables!")
             self.fill_tables()
-            # self.fill_tables()
-        # elif self.add_data:
-        #     self.fill_tables()
-        #     return
         else:
             return
         
     def __validate_inputs(self):
         '''
-        Validates a number of the inputs passed to the class to ensure they will not cause errors.
+        Validates the inputs passed to the class to ensure they will not cause errors.
         '''
         print('Validating Inputs...')
 
@@ -66,17 +73,23 @@ class MyDB:
         return
     
     def connect(self):
+        ''' create connection objects to the sqlite3 database (legislation.db)'''
         self.conn = sqlite3.connect(self.path_db, isolation_level=None, check_same_thread=False)
         self.curs = self.conn.cursor()
         return 
     
     def close(self):
+        'Close connection to legislation.db'
         self.conn.close()
         return
     
     def load_df(self):
         '''
-        Reads .csv files into a Pandas dataframe, and returns the dataframe.
+        Reads in all of the .csv files in the data folder 
+        and produces a single Pandas dataframe with all rows. 
+        
+        In this new dataframe, we add empty columns to log errors, text, and a datetime timestamp
+        for when we attempt to retrieve bill texts using Tika.
         ''' 
         path = './data'
         csv_files = glob.glob(path + "/*.csv")
@@ -136,7 +149,9 @@ class MyDB:
     
     def fill_tables(self):
         '''
-        Fills the pre-built tables with the data frome the provided .csv file.
+        Fills the pre-built tables with the data from the dataframe created by the load_df method.
+        
+        After creating the concatenated dataframe, we subset the dataframe to only include rows where the url column (what Tika will use to retrieve texts) is not empty. Then we group the entries by state.
 
         For each row, the function checks if the bill already exists in the database before adding it.
         '''
@@ -146,15 +161,17 @@ class MyDB:
         df = self.load_df()
         row_counter = 0
         row_input_lim = 0
-        
+        # subset of large df with only rows that have URLs
         df_not_nan = df.loc[df['url'].notnull()]
         
-        sample_df = df_not_nan.groupby(["state"]).groupby(["session"].sample(n=5, random_state=1)
+        # create a SAMPLE of the full dataset to include 5 bills from each state in the database (n= n bills per state)
+        # this parameter will not be hard coded in future versions
+        sample_df = df_not_nan.groupby(["state"]).sample(n=5, random_state=1)
 
         try:
-            for i, row in enumerate(sample_df.to_dict(orient='records')):
+            for i, row in enumerate(sample_df.to_dict(orient='records')): # create a dict with dataframe rows
                 if row_input_lim < self.input_lim:
-                    # Check if Bills exists
+                    # Check if Bills exists in database
                     x = pd.read_sql(SQ.SQL_CHECK_BILLS, self.conn, params=row)
                     if len(x) == 0:
                         # Insert the record if it did not
@@ -163,7 +180,7 @@ class MyDB:
                         print(row_counter)
                         clear_output(wait=True)
             
-            self.conn.commit()
+            self.conn.commit() # commit changes to database
             self.close()
 
         except Exception:
@@ -176,25 +193,26 @@ class MyDB:
 
         return
     
-    # def fill_table_chunks(self): 
-    #     PATH = './data/bills-with-urls.csv'
-    #     df = pd.read_csv(PATH)
-    #     sample_df = df.groupby(["state"]).sample(n=1, random_state=1)
-    #     chunk_counter = 0
-    #     self.connect()
-    #     for c in pd.read_csv(PATH, chunksize=self.chunk_size):
-    #         chunk_counter += 1 
-    #         c.to_sql(name="tBills", index=False,con=self.conn, chunksize=self.chunk_size, if_exists="append")
-    #         print(chunk_counter)
-    #         clear_output(wait=True)
-    #     self.close()
+    # this function is only intended to be used after this project is adapted
+    # to accomodate the size and runtime required to process all legislation data
+    def fill_table_chunks(self): 
+        PATH = './data/bills-with-urls.csv' # this file does not exist on github (~2.8 million rows)
+        df = pd.read_csv(PATH)
+        sample_df = df.groupby(["state"]).sample(n=1, random_state=1)
+        chunk_counter = 0
+        self.connect()
+        for c in pd.read_csv(PATH, chunksize=self.chunk_size):
+            chunk_counter += 1 
+            c.to_sql(name="tBills", index=False,con=self.conn, chunksize=self.chunk_size, if_exists="append")
+            print(chunk_counter)
+            clear_output(wait=True)
+        self.close()
     
         
     def get_tBills(self):
         '''
         Returns the tBills table from the provided database as a Pandas dataframe
         '''
-       
         sql = "SELECT * FROM tBills;"
         df = self.run_query(sql)
         return df
@@ -203,6 +221,8 @@ class MyDB:
                   sql: str, 
                   params: tuple|dict=None
                   ) -> pd.DataFrame:
+        '''Use this method to access data from the SQL database with your own query'''
+        
         self.connect()
         results = pd.read_sql(sql, self.conn, params=params)
         self.close()
